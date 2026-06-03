e79978d2737c9d22b95713b352eb0bcd9bbcb52c
Naol <naolmes15062015@gmail.com>
2026-06-04 01:39:55 +0300

    explore btn added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
+import ExploreBtn from "@/components/ExploreBtn"
+
 const Home = () => {
   console.log("server side log")
   return (
     <main>
-      <div>
-        <h1>Dev event</h1>
-      </div>
+      <section>
+        <h1 className="text-center">
+          The Hub for Every Dev <br />
+          Event You Can't Miss
+        </h1>
+        <p className="text-center mt-5">
+          Hackathons, Meetups, Conferences, All in One Place
+        </p>
+        <ExploreBtn />
+      </section>
     </main>
   )
 }
