72e08f00c7ffb0e3e80c8fa159090dcf20b24a01
Naol <naolmes15062015@gmail.com>
2026-06-05 14:23:06 +0300

    implementing  cloudinary to upload the  image

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,10 +1,12 @@
 import connectDB from "@/lib/mongodb"
+import { v2 as cloudinary } from "cloudinary"
 import { NextRequest, NextResponse } from "next/server"
 import Event from "@/database/event.model"
+import { resolve } from "path"
 export async function POST(req: NextRequest) {
   try {
     await connectDB()
-    /*
+
     const formData = await req.formData()
     let event
 
@@ -17,9 +19,40 @@ export async function POST(req: NextRequest) {
         },
         { status: 400 },
       )
-    }*/
-    const body = await req.json()
-    const createdEvent = await Event.create(body)
+    }
+    const file = formData.get("image") as File
+
+    if (!file) {
+      return NextResponse.json(
+        {
+          message: "Image file is requires",
+        },
+        {
+          status: 400,
+        },
+      )
+    }
+
+    const arrayBuffer = await file.arrayBuffer()
+    const buffer = Buffer.from(arrayBuffer)
+    const uploadResult = await new Promise((resolve, reject) => {
+      cloudinary.uploader
+        .upload_stream(
+          {
+            resource_type: "image",
+            folder: "DevEvent",
+          },
+          (error, results) => {
+            if (error) return reject(error)
+            // if (results) return
+            resolve(results)
+          },
+        )
+        .end(buffer)
+    })
+
+    event.image = (uploadResult as { secure_url: string }).secure_url
+    const createdEvent = await Event.create(event)
     return NextResponse.json(
       {
         message: "Event Created Successfully",
