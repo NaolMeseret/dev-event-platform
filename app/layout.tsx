2f274afa8625c9741f9c6a43354ab1688fb0449c
Naol <naolmes15062015@gmail.com>
2026-06-08 22:41:03 +0300

    finalize the app

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,10 @@ import { cn } from "@/lib/utils"
 import LightRays from "@/components/LightRays"
 import NavBar from "@/components/NavBar"
 import { PostHogProvider } from "@/components/PostHogProvider"
-import { Suspense } from "react"
+// import { Suspense } from "react"
 import { PostHogPageView } from "@/components/PostHogPageView"
 const geist = Geist({ subsets: ["latin"], variable: "--font-sans" })
+import { Suspense } from "react"
 
 const schibstedGrotesk = Schibsted_Grotesk({
   variable: "--font-schibsted-grotesk",
@@ -42,30 +43,32 @@ export default function RootLayout({
       )}
     >
       <body className="min-h-full flex flex-col">
-        <PostHogProvider>
-          <Suspense>
+        <Suspense fallback={null}>
+          <PostHogProvider>
+            {/* <Suspense> */}
             <PostHogPageView />
-          </Suspense>
-          <NavBar />
-          <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
-            <LightRays
-              raysOrigin="top-center-offset"
-              raysColor="#5dfeca"
-              raysSpeed={0.5}
-              lightSpread={0.9}
-              rayLength={1.4}
-              followMouse={true}
-              mouseInfluence={0.02}
-              noiseAmount={0}
-              distortion={0.01}
-              className="custom-rays"
-              pulsating={false}
-              fadeDistance={1}
-              saturation={1}
-            />
-          </div>
-          <main>{children}</main>
-        </PostHogProvider>
+            {/* </Suspense> */}
+            <NavBar />
+            <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
+              <LightRays
+                raysOrigin="top-center-offset"
+                raysColor="#5dfeca"
+                raysSpeed={0.5}
+                lightSpread={0.9}
+                rayLength={1.4}
+                followMouse={true}
+                mouseInfluence={0.02}
+                noiseAmount={0}
+                distortion={0.01}
+                className="custom-rays"
+                pulsating={false}
+                fadeDistance={1}
+                saturation={1}
+              />
+            </div>
+            <main>{children}</main>
+          </PostHogProvider>
+        </Suspense>
       </body>
     </html>
   )
